eb0ff8a886b9469e933d1f390789cf5fd06af240
Welton Quirino <wpquirino@gmail.com>
2025-09-16 19:48:04 -0300

    termino manipulacao e limpeza

--- limpeza.py
+++ limpeza.py
@@ -1,28 +1,29 @@
 #%%
 import pandas as pd
-import numpy as np
-
 dados = pd.read_csv('titanic.csv')
 #%%
-dados['Age'].isnull().sum()
-dados['Age'] = dados['Age'].fillna(27)
-dados.Age.unique()
-dados['Age'] = dados['Age'].astype(int)
+dados.columns = dados.columns.str.upper()
+dados['AGE'] = dados['AGE'].fillna(dados['AGE'].median())
+dados['AGE'] = dados['AGE'].astype(int)
+#%%
+dados_cabine = dados[dados['CABIN'].notna()] .copy()
 
 #%%
-dados_cabine = dados[dados['Cabin'].notna()] .copy()
-dados_cabine
+dados.drop(['CABIN'], axis=1, inplace=True)
 #%%
-dados.drop(['Cabin'], axis=1, inplace=True)
+dados['FARE'] = dados['FARE'].fillna(dados['FARE'].median())
 #%%
-import numpy as np
-dados['tamanho_familia'] = dados['SibSp'] + dados['Parch'] + 1 #Criando uma coluna com o tamanho da familia
-dados['Fare_por_pessoa'] = dados['Fare'] / dados['tamanho_famailia'] #criando uma coluna com a taxa por pessoa
+dados['FAMILY_SIZE'] = dados['SIBSP'] + dados['PARCH'] + 1 #Criando uma coluna com o tamanho da familia
+dados['FARE_PER_PERSON'] = dados['FARE'] / dados['FAMILY_SIZE'] #criando uma coluna com a taxa por pessoa
 
 #%%
 #Analisar a variancia entre as taxas, qual a menor a maior e quanto varia
-dados_taxa = dados[['tamanho_familia','Fare_por_pessoa']].copy()
+dados_taxa = dados[['FAMILY_SIZE','FARE_PER_PERSON']].copy()
 #%%
-dados[['Name','Sex']] = dados[['Name','Sex']].apply(lambda x: x.str.upper())
-dados.columns = dados.columns.str.upper()
-dados
+dados[['NAME','SEX']] = dados[['NAME','SEX']].apply(lambda x: x.str.upper())
+
+#%%
+#proximos passos criar uma coluna de faixas para o Fare
+#Os nulos da coluna Age, pegar a mediana do masculino e Feminino
+dados['FAIXAS_FARE'] = pd.qcut(dados['FARE'], q=4, labels=['BAIXO','MÉDIO','ALTO','MUITO ALTO'])
+dados.info()
